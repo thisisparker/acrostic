7e994ff3dc5ceb9052b52e17a445693d05d5baec
Parker Higgins <parker@parkerhiggins.net>
2020-04-25 12:27:24 -0400

    cleans up structure a little, adds a first stab at sorting words for extraction

--- acrostic.py
+++ acrostic.py
@@ -1,19 +1,14 @@
 from collections import Counter
 
 import random
+import sys
 
-quote = "Whenever I find myself growing grim about the mouth; whenever it is a damp, drizzly November in my soul; whenever I find myself involuntarily pausing before coffin warehouses, and bringing up the rear of every funeral I meet; and especially whenever my hypos get such an upper hand of me, that it requires a strong moral principle to prevent me from deliberately stepping into the street, and methodically knocking people’s hats off—then, I account it high time to get to sea as soon as I can."
+QUOTE = "Whenever I find myself growing grim about the mouth; whenever it is a damp, drizzly November in my soul; whenever I find myself involuntarily pausing before coffin warehouses, and bringing up the rear of every funeral I meet; and especially whenever my hypos get such an upper hand of me, that it requires a strong moral principle to prevent me from deliberately stepping into the street, and methodically knocking people’s hats off—then, I account it high time to get to sea as soon as I can."
 
-possible_subs = ['hermanmelville', 'mobydick']
+AUTHOR = 'Herman Melville'
+WORK = 'Moby Dick'
 
-with open('prepared-dict.txt') as f:
-    words = [word.strip() for word in f.readlines()]
-
-random.shuffle(words)
-
-possible_subs.extend(words)
-
-subs = []
+FREQ_ALPH = 'qjxzwkvfybhmpgudclotnrsaie'
 
 class Passage():
     def __init__(self, text):
@@ -46,11 +41,34 @@ class Passage():
 
 
 def main():
-    p = Passage(quote)
+    p = Passage(QUOTE)
 
-    print('Beginning with the passage:')
+    author = ''.join([l for l in AUTHOR.lower() if l.isalpha()])
+    work = ''.join([l for l in WORK.lower() if l.isalpha()])
 
-    print(p.display)
+    if not (p.is_substring(author) and p.is_substring(work)):
+        sys.exit('Passage does not contain anagrams for {} and {}.'.format(author, work))
+
+    with open('prepared-dict.txt') as f:
+        words = [word.strip() for word in f.readlines()]
+
+    words = sorted(words, key=lambda word: [FREQ_ALPH.find(l) for l in word])
+    shuffled_words = []
+
+    for x in range(0, len(words), int(len(words)/10)):
+        decile = words[x:x+int(len(words)/10)]
+        random.shuffle(decile)
+        shuffled_words.extend(decile)
+
+    possible_subs = shuffled_words
+
+    subs = []
+
+    print('Finding an acrostic for the passage:', end='\n\n')
+
+    print(p.display, end='\n\n')
+
+    print('from {} by {}.'.format(WORK, AUTHOR), end='\n\n')
 
     for sub in possible_subs:
         if p.is_substring(sub):
@@ -60,12 +78,12 @@ def main():
             pass
             # print(sub, 'is not a substring')
 
-    print('You may remove the anagrammed substrings:')
-    print(subs)
+    print('You may remove the anagrammed substrings:', end='\n\n')
+    print(subs, end='\n\n')
 
     remaining = ''.join([l for l in p.display if l.isalpha()])
 
-    print('With {} letters remaining:'.format(len(remaining)))
+    print('With {} letters remaining:'.format(len(remaining)), end='\n\n')
     print(remaining)
 
 if __name__ == '__main__':
